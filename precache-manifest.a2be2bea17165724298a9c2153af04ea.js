82d3e813810e88fd81b3794ea4964ee63f3fbc76
Kat Tochowicz <ktochow1@binghamton.edu>
2020-10-07 13:52:49 +0200

    Updates

---
+++ precache-manifest.a2be2bea17165724298a9c2153af04ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac6685129942bae5400215df6572c34",
+    "url": "/monsters-rolodex2/index.html"
+  },
+  {
+    "revision": "442d7b1461cae755f5f9",
+    "url": "/monsters-rolodex2/static/css/main.1f8f1a70.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex2/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex2/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442d7b1461cae755f5f9",
+    "url": "/monsters-rolodex2/static/js/main.16fc239b.chunk.js"
+  },
+  {
+    "revision": "5129b2734e446f0ec2c6",
+    "url": "/monsters-rolodex2/static/js/runtime-main.e8342c1e.js"
+  }
+]);
